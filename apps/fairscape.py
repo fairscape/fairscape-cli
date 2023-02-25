fce20522ae154275bc8fd1fd88f77e80ff1c9efb
ma3xy <ma3xy@virginia.edu>
2023-02-24 21:18:12 -0400

    multiple options added for describe command

--- apps/fairscape.py
+++ apps/fairscape.py
@@ -12,5 +12,16 @@ app.add_typer(apps.validator.app, name="validate")
 app.add_typer(apps.list.app, name="list")
 app.add_typer(apps.describe.app, name="describe")
 
+
+@app.command("describe-id")
+def describe_id(id: str = typer.Option("", help="Describe identifier metadata from id e.g. ark:5982/UVA/b2ai")):
+    pass
+
+
+@app.command("describe-name")
+def describe_id(name: str = typer.Option("", help="Describe identifier metadata from name e.g. b2ai computation")):
+    pass
+
+
 if __name__ == "__main__":
     app()
