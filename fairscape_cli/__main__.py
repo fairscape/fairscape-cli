02b71a99395f803db3f1ec36c62f99582c053ece
mlev71 <max.adam.levinson@gmail.com>
2023-03-27 13:00:53 -0400

    moving main application to cli.py

--- fairscape_cli/__main__.py
+++ fairscape_cli/__main__.py
@@ -1,22 +1,4 @@
-import click
-
-from fairscape_cli.validate import validate_json
-from fairscape_cli.rocrate import rocrate
-#from fairscape_cli.client import client
-
-@click.group()
-def cli():
-    pass
-
-# Validation Subcommands
-cli.add_command(validate_json.validate)
-
-# ROCrate Subcommands
-cli.add_command(rocrate.rocrate)
-
-# Fairscape Client Commands
-# cli.add_command(client)
-
+from fairscape_cli.cli import cli
 
 if __name__ == "__main__":
     cli()
