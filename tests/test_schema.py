a3ff8bc08dd28db673590c3a59ebbf1dd1395bae
mlev71 <max.adam.levinson@gmail.com>
2023-04-28 10:51:24 -0400

    added test for APMS node2vec embeddings

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -189,7 +189,7 @@ class TestValidateTabular():
 
         gene_id_column = ColumnSchema(
             name = "GeneID",
-            description = "Internal Gene ID for Lundberg Lab Bait Gene",
+            description = "Internal Gene ID for Gygi Lab Bait Gene",
             datatype = "integer",
             number = 1,
             required = True,
@@ -199,7 +199,7 @@ class TestValidateTabular():
 
         num_interactors_column = ColumnSchema(
             name = "# Interactors",
-            description = "number of prey proteints attracted to the bait protien",
+            description = "number of prey proteins attracted to the bait protien",
             datatype = DatatypeSchema(base="integer", minimum=0),
             number = 2,
             required = True,
@@ -235,7 +235,9 @@ class TestValidateTabular():
 		"evi": "https://w3id.org/EVI",
 		"@vocab": "https://schema.org"
             },
-            description = "Schema for Lundberg Lab Baitlist files",
+            seperator= ",",
+            description = "Schema for Gygi Lab Baitlist files",
+            header = True,
             columns = [
                 gene_symbol_column,
                 gene_id_column,
@@ -243,9 +245,67 @@ class TestValidateTabular():
             ] 
         )
 
+        tabular_data_test.validate(path=pathlib.Path("./tests/data/baitlist.csv")
 
-    def test_success(self):
-        pass
+
+    def test_APMS_embedding(self):
+
+        apms_datatype = DatatypeSchema(
+            name = "APMS Experiment",
+            description = "Identifier for APMS experiment corresponding to the given node2vec vector",
+            base = "string",
+            format = "APMS_[0-9]*"
+        )
+
+        apms_column = ColumnSchema(
+            name = "APMS Experiment",
+            description = "APMS_column",
+            ordered = False,
+            required = True,
+            number = 0,
+            datatype = apms_datatype,
+            titles = ["APMS Experiment"] 
+        )
+
+        gene_symbol_datatype = DatatypeSchema(
+            name = "Gene Symbol",
+            description = "Gene Symbol in String Form",
+            base = "string",
+            format = "[A-Z0-9]*",
+            minLength = 3,
+            maxLength = 20
+        )
+
+        gene_symbol_column = ColumnSchema(
+            name = "Gene Symbol",
+            description = "gene symbol for apms embedding vector",
+            ordered = False,
+            required = True,
+            number = 0,
+            datatype = gene_symbol_datatype,
+            valueURL = "http://edamontology.org/data_1026",
+            titles = ["Gene Symbol"] 
+        )
+
+        embedding_column = ColumnSchema(
+            name = "embedding values",
+            description = "node2vec embedding vector values for genes",
+            number = "2::",
+            titles = ["node2vec embedding vector"]
+        ) 
+
+        embedding_schema = TabularDataSchema(
+            guid = "ark:99999/schema/apms_embedding_schema",
+            name = "apms embedding schema",
+            description = "embedding vector values for genes determined by running node2vec on APMS networks",
+            seperator = ",",
+            header = False,
+            columns = [
+                apms_column,
+                gene_column,
+                embedding_column
+            ]
+        )
 
 
 class TestSchemaCLI():
