c84d774ee9c53d78fadf006e81b3aacc0627c7fc
mlev71 <max.adam.levinson@gmail.com>
2023-05-09 14:00:02 -0400

    implementing register command, using new ROCrate class methods

--- fairscape_cli/models/rocrate.py
+++ fairscape_cli/models/rocrate.py
@@ -27,7 +27,8 @@ class ROCrate(ROCrateModel):
         
 
     def initCrate(self):
-        """Create an rocrate at the current working directory, initilize the 
+        """Create an rocrate at the current working directory, initilize the ro-crate-metadata.json
+
         """
 
         # TODO url encode all string values for organization_name and project_name
@@ -77,18 +78,69 @@ class ROCrate(ROCrateModel):
             json.dump(rocrate_metadata, metadata_file, indent=2)
 
         #TODO add to cache
+
+        #TODO list all contents that need to be registered as warnings   
+
+ 
+    def copyObject(source_filepath: str, destination_filepath: str):
+
+        if source_filepath == "":
+            raise Exception(message="source path is None")
+
+        if destination_filepath == "":
+            raise Exception(message="destination path is None") 
+
+        # check if the source file exists 
+        source_path = pathlib.Path(source_filepath)
+        destination_path = pathlib.Path(destination_filepath)
+
+        if source_path.exists() != True:
+            raise Exception(
+                message =f"sourcePath: {source_path} Doesn't Exist"
+            )
+    
+        # TODO check that destination path is in the rocrate
+
+        # copy the file into the destinationPath
+        shutil.copy(source_path, destination_path)
+
+
+
+    def registerObject(self, model: Union[Dataset, Software, Computation]):
+        ''' Add a specified peice of metadata to the graph of an ROCrate
+
+        Marshals a given model into JSON-LD, opens the ro-crate-metadata.json,
+        appends the new metadata to the @graph, and overwrites the ro-crate-metadata.json
+        '''
+
+        metadata_path = self.path
+
+        # open the ro-crate-metadata.json
+        with metadata_path.open("r") as rocrate_metadata_file:
+            rocrate_metadata = json.load(rocrate_metadata_file)
+
+            # TODO check if the file is redundant
      
+            # add to the @graph
+            rocrate_metadata['@graph'].append(model.dict(by_alias=True))
+        
+        # overwrite the ro-crate-metadata.json file
+        with metadata_path.open("w") as f:
+            json_object = json.dumps(rocrate_metadata, indent=2)
+            f.write(json_object)
 
-    def addDataset(self, Dataset):
-        pass
 
 
-    def addSoftware(self, Software):
-        pass
+    def registerDataset(self, Dataset):
+        self.registerObject(model=Dataset)
+        
+
+    def registerSoftware(self, Software):
+        self.registerObject(model=Software)
 
 
-    def addComputation(self, Computation):
-        pass
+    def registerComputation(self, Computation):
+        self.registerComputation(model=Computation)
 
 
     def listContents(self):
--- fairscape_cli/rocrate/rocrate.py
+++ fairscape_cli/rocrate/rocrate.py
@@ -148,8 +148,220 @@ def create(
 # RO Crate add subcommands
 ##########################
 
-def add_element_ro_crate(element: Union[Dataset,Computation,Software], ro_crate: pathlib.Path):
+def add_element_ro_crate(
+    element: Union[Dataset,Computation,Software], 
+    ro_crate: pathlib.Path
+    ):
     pass
+
+
+@rocrate.group('register')
+def register():
+    pass
+
+@register.command('software')
+@click.argument('rocrate-path', type=click.Path(exists=True, path_type=pathlib.Path))
+@click.option('--guid', required=False, type=str, default="", show_default=False)
+@click.option('--name',    required=True, prompt = "Software Name")
+@click.option('--author',  required=True, prompt = "Author Name")
+@click.option('--version', required=True, prompt = "Software Version")
+@click.option('--description', required = True, prompt = "Software Description")
+@click.option('--file-format', required = True, prompt = "File Format of Software")
+@click.option('--url',     required = False)
+@click.option('--date-modified', required=False)
+@click.option('--filepath', required=True)
+@click.option('--used-by-computation', required=False, multiple=True)
+@click.option('--associated-publication', required=False)
+@click.option('--additional-documentation', required=False)
+def registerSoftware(
+    rocrate_path: pathlib.Path,
+    guid: str,
+    name: str,
+    author: str,
+    version: str,
+    description: str, 
+    file_format: str,
+    url: str,
+    date_modified: str,
+    filepath: str,
+    used_by_computation: Optional[List[str]],
+    associated_publication: Optional[str],
+    additional_documentation: Optional[str]
+    ):
+
+    
+    metadata_path = rocrate_path / "ro-crate-metadata.json"
+
+    # check if you are in the rocrate path
+    # ro-crate-metadata.json should be a local file
+    if metadata_path.exists() != True:
+        click.echo(f"Cannot Find RO-Crate Metadata: {metadata_path}")
+        click.Abort()
+
+    crate = ROCrate(path=metadata_path)
+
+    try:
+        software_model = Software(   
+            **{
+            "@id": guid,
+            "@type": "https://w3id.org/EVI#Software",
+            "url": url,
+            "name": name,
+            "author": author,
+            "dateModified": date_modified,
+            "description": description,
+            "version": version,
+            "associatedPublication": associated_publication,
+            "additionalDocumentation": additional_documentation,
+            "format": file_format,
+            "usedByComputation": used_by_computation,
+            "contentUrl": "file://" + filepath
+            }
+        )
+
+    except ValidationError as e:
+        click.echo("Software Validation Error")
+        click.echo(e)
+        click.Abort()
+        
+    crate.registerSoftware(software_model)
+ 
+    click.echo(guid)
+
+
+@register.command('dataset')
+@click.argument('rocrate-path', type=click.Path(exists=True, path_type=pathlib.Path))
+@click.option('--guid', required=False, default="", type=str)
+@click.option('--name', required=True, prompt="Dataset Name")
+@click.option('--url', required=False)
+@click.option('--author', required=True, prompt="Dataset Author")
+@click.option('--version', required=True, prompt="Dataset Version")
+@click.option('--date-published', required=True, prompt="Date Published")
+@click.option('--description', required=True, prompt="Dataset Description")
+@click.option('--data-format', required=True, prompt="Data Format i.e. (csv, tsv)")
+@click.option('--filepath', required=True)
+@click.option('--used-by', required=False, multiple=True)
+@click.option('--derived-from', required=False, multiple=True)
+@click.option('--associated-publication', required=False)
+@click.option('--additional-documentation', required=False)
+def registerDataset(
+    rocrate_path: pathlib.Path,
+    guid: str,
+    name: str,
+    url: str,
+    author: str,
+    description: str,
+    date_published: str,
+    version: str,
+    associated_publication: Optional[str],
+    additional_documentation: Optional[List[str]],
+    data_format: str,
+    filepath: str,
+    derived_from: Optional[List[str]],
+    used_by: Optional[List[str]],
+):
+    
+    metadata_path = rocrate_path / "ro-crate-metadata.json"
+
+    # check if you are in the rocrate path
+    # ro-crate-metadata.json should be a local file
+    if metadata_path.exists() != True:
+        click.echo(f"Cannot Find RO-Crate Metadata: {metadata_path}")
+        click.Abort()
+
+    crate = ROCrate(path=metadata_path)
+
+    try:
+        dataset_model = Dataset(   
+            **{
+            "@id": guid,
+            "@type": "https://w3id.org/EVI#Dataset",
+            "url": url,
+            "author": author,
+            "name": name,
+            "description": description,
+            "datePublished": date_published,
+            "version": version,
+            "associatedPublication": associated_publication,
+            "additionalDocumentation": additional_documentation,
+            "format": data_format,
+            "derivedFrom": derived_from,
+            "usedBy": used_by
+            }
+        )
+
+    except ValidationError as e:
+        click.echo("Dataset Validation Error")
+        click.echo(e)
+        click.Abort()
+    
+    crate.registerSoftware(software_model)
+ 
+    click.echo(guid)
+
+
+@register.command('computation')
+@click.argument('rocrate-path', type=click.Path(exists=True, path_type=pathlib.Path))
+@click.option('--guid', required=False, default="", type=str, show_default=False)
+@click.option('--name', required=True, prompt="Computation Name")
+@click.option('--run-by', required=True, prompt="Computation Run By")
+@click.option('--command', required=False, prompt="Command")
+@click.option('--date-created', required=True, prompt="Date Created")
+@click.option('--description', required=True, prompt="Computation Description")
+@click.option('--used-software', required=False, multiple=True)
+@click.option('--used-dataset', required=False, multiple=True)
+@click.option('--generated', required=False, multiple=True)
+def computation(
+    rocrate_path: pathlib.Path,
+    guid: str,
+    name: str,
+    run_by: str,
+    command: Optional[Union[str, List[str]]],
+    date_created: str,
+    description: str,
+    used_software,
+    used_dataset,
+    generated
+):
+    metadata_path = rocrate_path / "ro-crate-metadata.json"
+    # check if you are in the rocrate path
+    # ro-crate-metadata.json should be a local file
+    if metadata_path.exists() != True:
+        click.echo(f"Cannot Find RO-Crate Metadata: {metadata_path}")
+        click.Abort()
+
+    if guid == "":
+        guid = generate_id(metadata_path, name, "Computation")
+
+    # initilize the model with the required properties
+    try:
+
+        computation_model = Computation(   
+            **{
+            "@id": guid,
+            "@type": "https://w3id.org/EVI#Computation",
+            "name": name,
+            "runBy": run_by,
+            "dateCreated": date_created,
+            "description": description,
+            "usedSoftware": used_software,
+            "usedDataset": used_dataset,
+            "generated": generated,
+            }
+        )
+    except ValidationError as e:
+        click.echo("Computation Validation Error")
+        click.echo(e)
+        click.Abort()
+
+    crate.registerComputation(dataset_model)
+    
+    click.echo(guid)
+        #click.echo("Added Computation")
+        #click.echo(
+        #    json.dumps(computation_model.dict(by_alias=True), indent=2)
+        #)
+
 
 # RO Crate add subcommands
 @rocrate.group('add')
@@ -166,8 +378,8 @@ def add():
 @click.option('--description', required = True, prompt = "Software Description")
 @click.option('--file-format', required = True, prompt = "File Format of Software")
 @click.option('--url',     required = False)
-@click.option('--source-filepath', required=False)
-@click.option('--destination-filepath', required=False)
+@click.option('--source-filepath', required=True)
+@click.option('--destination-filepath', required=True)
 @click.option('--date-modified', required=False)
 @click.option('--used-by-computation', required=False, multiple=True)
 @click.option('--associated-publication', required=False)
@@ -189,7 +401,6 @@ def software(
     additional_documentation: Optional[str]
 ):
 
-
     
     metadata_path = rocrate_path / "ro-crate-metadata.json"
 
@@ -202,84 +413,44 @@ def software(
     if guid == "":
         guid = generate_id(metadata_path, name, "Software")
 
+    
+    crate = ROCrate(path=metadata_path)
 
-    # initilize the model with the required properties
+
     try:
-
-        # if moving local files
-        if source_filepath !=  "" and destination_filepath != "":
-
-            # check if the source file exists 
-            source_path = pathlib.Path(source_filepath)
-            destination_path = pathlib.Path(destination_filepath)
-
-            if source_path.exists() != True:
-                click.echo(f"sourcePath: {source_path} Doesn't Exist")
-                click.Abort() 
-            
-            # TODO check that destination path is in the rocrate
-
-            software_model = Software(   
-                **{
-                "@id": guid,
-                "@type": "https://w3id.org/EVI#Software",
-                "url": url,
-                "name": name,
-                "author": author,
-                "dateModified": date_modified,
-                "description": description,
-                "version": version,
-                "associatedPublication": associated_publication,
-                "additionalDocumentation": additional_documentation,
-                "format": file_format,
-                "usedByComputation": used_by_computation,
-                "contentUrl": "file://" + str(destination_path)
-                }
-            )
-
-            # copy the file into the destinationPath
-            shutil.copy(source_path, destination_path)
-
-        else:
-            software_model = Software(   
-                **{
-                "@id": guid,
-                "@type": "https://w3id.org/EVI#Software",
-                "url": url,
-                "name": name,
-                "author": author,
-                "dateModified": date_modified,
-                "description": description,
-                "version": version,
-                "associatedPublication": associated_publication,
-                "additionalDocumentation": additional_documentation,
-                "format": file_format,
-                "usedByComputation": used_by_computation
-                }
-            )
-            
-        # open the ro-crate-metadata.json
-        with metadata_path.open("r") as rocrate_metadata_file:
-            rocrate_metadata = json.load(rocrate_metadata_file)
-
-            # TODO check if the file is redundant
-     
-            # add to the @graph
-            rocrate_metadata['@graph'].append(software_model.dict(by_alias=True))
-        
-        # overwrite the ro-crate-metadata.json file
-        with metadata_path.open("w") as f:
-            json_object = json.dumps(rocrate_metadata, indent=2)
-            f.write(json_object)
-
-        click.echo(guid)
-
+        software_model = Software(   
+            **{
+            "@id": guid,
+            "@type": "https://w3id.org/EVI#Software",
+            "url": url,
+            "name": name,
+            "author": author,
+            "dateModified": date_modified,
+            "description": description,
+            "version": version,
+            "associatedPublication": associated_publication,
+            "additionalDocumentation": additional_documentation,
+            "format": file_format,
+            "usedByComputation": used_by_computation,
+            "contentUrl": "file://" + str(destination_path)
+            }
+        )
 
     except ValidationError as e:
         click.echo("Software Validation Error")
         click.echo(e)
         click.Abort()
 
+    crate.registerSoftware(software_model)
+
+    try:
+        crate.copyObject(source_filepath, destination_filepath)
+    except:
+        click.echo("Failed to Copy Software")
+        click.echo(e)
+        click.Abort()
+
+    click.echo(guid)
 
     # TODO add to cache
 
@@ -331,164 +502,41 @@ def dataset(
         guid = generate_id(metadata_path, name, "Dataset")
 
 
+    crate = ROCrate(path=metadata_path)
     
-    # initilize the model with the required properties
     try:
-
-        if destination_filepath != "" and source_filepath != "":
-            
-
-            # TODO check that destination path is in the rocrate
-            destination_path = pathlib.Path(destination_filepath)
-            source_path = pathlib.Path(source_filepath) 
-
-            # check if the source file exists 
-            if source_path.exists() != True:
-                click.echo(f"sourcePath: {sourcePath} Doesn't Exist")
-                clic.Abort()
-
-            dataset_model = Dataset(   
-                **{
-                "@id": guid,
-                "@type": "https://w3id.org/EVI#Dataset",
-                "url": url,
-                "author": author,
-                "name": name,
-                "description": description,
-                "datePublished": date_published,
-                "version": version,
-                "associatedPublication": associated_publication,
-                "additionalDocumentation": additional_documentation,
-                "format": data_format,
-                "derivedFrom": derived_from,
-                "usedBy": used_by,
-                "contentUrl": "file://" + str(destination_path)
-                }
-            )
-
-            # copy the file into the destinationPath
-            shutil.copy(source_path, destination_path)
-
-        else:
-            dataset_model = Dataset(   
-                **{
-                "@id": guid,
-                "@type": "https://w3id.org/EVI#Dataset",
-                "url": url,
-                "author": author,
-                "name": name,
-                "description": description,
-                "datePublished": date_published,
-                "version": version,
-                "associatedPublication": associated_publication,
-                "additionalDocumentation": additional_documentation,
-                "format": data_format,
-                "derivedFrom": derived_from,
-                "usedBy": used_by
-                }
-            )
-        
-
-        # open the ro-crate-metadata.json
-        with metadata_path.open("r") as rocrate_metadata_file:
-            rocrate_metadata = json.load(rocrate_metadata_file)
-
-        # TODO check if the file is redundant
-     
-        # add to the @graph
-        rocrate_metadata['@graph'].append(dataset_model.dict(by_alias=True))
-        
-        # overwrite the ro-crate-metadata.json file
-        with metadata_path.open("w") as f:
-            json.dump(rocrate_metadata, f, indent=2)
-
-        click.echo(guid)
+        dataset_model = Dataset(   
+            **{
+            "@id": guid,
+            "@type": "https://w3id.org/EVI#Dataset",
+            "url": url,
+            "author": author,
+            "name": name,
+            "description": description,
+            "datePublished": date_published,
+            "version": version,
+            "associatedPublication": associated_publication,
+            "additionalDocumentation": additional_documentation,
+            "format": data_format,
+            "derivedFrom": derived_from,
+            "usedBy": used_by,
+            "contentUrl": "file://" + str(destination_path)
+            }
+        )
 
     except ValidationError as e:
         click.echo("Software Validation Error")
         click.echo(e)
         click.Abort()
-    
+
+    crate.registerDataset(dataset_model)
+    crate.copyObject(source_filepath, destination_filepath)
+
+    click.echo(guid) 
 
     # TODO add to cache
     
 
 
-@add.command('computation')
-@click.argument('rocrate-path', type=click.Path(exists=True, path_type=pathlib.Path))
-@click.option('--guid', required=False, default="", type=str, show_default=False)
-@click.option('--name', required=True, prompt="Computation Name")
-@click.option('--run-by', required=True, prompt="Computation Run By")
-@click.option('--command', required=False, prompt="Command")
-@click.option('--date-created', required=True, prompt="Date Created")
-@click.option('--description', required=True, prompt="Computation Description")
-@click.option('--used-software', required=False, multiple=True)
-@click.option('--used-dataset', required=False, multiple=True)
-@click.option('--generated', required=False, multiple=True)
-def computation(
-    rocrate_path: pathlib.Path,
-    guid: str,
-    name: str,
-    run_by: str,
-    command: Optional[Union[str, List[str]]],
-    date_created: str,
-    description: str,
-    used_software,
-    used_dataset,
-    generated
-):
 
-    metadata_path = rocrate_path / "ro-crate-metadata.json"
-    # check if you are in the rocrate path
-    # ro-crate-metadata.json should be a local file
-    if metadata_path.exists() != True:
-        click.echo(f"Cannot Find RO-Crate Metadata: {metadata_path}")
-        click.Abort()
-
-    if guid == "":
-        guid = generate_id(metadata_path, name, "Computation")
-
-    # initilize the model with the required properties
-    try:
-
-        computation_model = Computation(   
-            **{
-            "@id": guid,
-            "@type": "https://w3id.org/EVI#Computation",
-            "name": name,
-            "runBy": run_by,
-            "dateCreated": date_created,
-            "description": description,
-            "usedSoftware": used_software,
-            "usedDataset": used_dataset,
-            "generated": generated,
-            }
-        )
-
-        # open the ro-crate-metadata.json
-        with metadata_path.open("r") as rocrate_metadata_file:
-            rocrate_metadata = json.load(rocrate_metadata_file)
-
-        # TODO check if the file is redundant
-     
-        # add to the @graph
-        graph_metadata = rocrate_metadata.get('@graph', [])
-        graph_metadata.append(computation_model.dict(by_alias=True))
-        rocrate_metadata['@graph'] = graph_metadata
-        
-        # overwrite the ro-crate-metadata.json file
-        with metadata_path.open("w") as rocrate_metadata_file:
-            json.dump(rocrate_metadata, rocrate_metadata_file, indent=2)
-
-        click.echo(guid)
-        #click.echo("Added Computation")
-        #click.echo(
-        #    json.dumps(computation_model.dict(by_alias=True), indent=2)
-        #)
-
-
-    except ValidationError as e:
-        click.echo("Computation Validation Error")
-        click.echo(e)
-        click.Abort()
 
