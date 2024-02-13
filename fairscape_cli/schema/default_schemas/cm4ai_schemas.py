2be278fa2725c9adb7777a43ca105b7481a65b05
mlev71 <max.adam.levinson@gmail.com>
2024-02-13 16:13:04 -0500

    adding default schemas as raw data files

---
+++ fairscape_cli/schema/default_schemas/cm4ai_schemas.py
@@ -0,0 +1,15 @@
+from fairscape_cli.models.schema.utils import (
+        ReadSchema
+)
+
+cm4ai_schema_directory = '/'
+
+apms_loader_edgelist_schema = 'cm4ai_schema_apmsloader_ppi_edgelist.json'
+apms_loader_gene_node_schema =  'cm4ai_schema_apmsloader_ppi_gene_node_attributes.json'
+apms_embedding_schema = 'cm4ai_schema_apms_embedding.json'
+image_loader_samplescopy_schema = '../cm4ai-schemas/v0.1.0/cm4ai_schema_imageloader_samplescopy.json'
+image_loader_uniquecopy_schema = '../cm4ai-schemas/v0.1.0/cm4ai_schema_imageloader_uniquecopy.json'
+image_embedding_labels_schema = '../cm4ai-schemas/v0.1.0/cm4ai_schema_image_embedding_labels_prob.json'
+image_embedding_emd_schema = '../cm4ai-schemas/v0.1.0/cm4ai_schema_image_embedding_emd.json'
+coembedding_schema = '../cm4ai-schemas/v0.1.0/cm4ai_schema_coembedding.json'
+
