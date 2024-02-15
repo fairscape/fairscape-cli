702f45214ebf191b43f6788791d035c0e274803d
mlev71 <max.adam.levinson@gmail.com>
2024-02-15 17:04:44 -0500

    removing stale code

--- fairscape_cli/apps/list.py
+++
@@ -1,8 +0,0 @@
-import typer
-
-app = typer.Typer()
-
-
-@app.command("ROcrate")
-def list_rocrate():
-    pass
--- fairscape_cli/apps/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from fairscape_cli.apps.models.computation import Computation
-from fairscape_cli.apps.models.software import Software
-from fairscape_cli.apps.models.dataset import Dataset
-
