ff4425118e451e1cf80bc6df516b9d85b444165d
Max Levinson <max.adam.levinson@gmail.com>
2024-02-26 17:27:05 -0500

    GenerateDatetimeSquid method

--- fairscape_cli/models/utils.py
+++ fairscape_cli/models/utils.py
@@ -13,6 +13,15 @@ from fairscape_cli.config import (
 
 squids = Sqids(min_length=6)
 
+def GenerateDatetimeSquid():
+    try:
+        timestamp_int = int(datetime.datetime.now(datetime.UTC).timestamp())
+        sq = squids.encode([timestamp_int])
+    except: 
+        timestamp_int = int(datetime.datetime.utcnow().timestamp())
+        sq = squids.encode([timestamp_int])
+
+    return sq
 
 
 def GenerateDatetimeGUID(prefix: str)->str:
