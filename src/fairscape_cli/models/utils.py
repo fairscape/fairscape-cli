221acb6e03e87cfdca93370c346b63b730163225
Max Levinson <max.adam.levinson@gmail.com>
2024-10-29 16:18:53 -0400

    updating sqids to have random element

--- src/fairscape_cli/models/utils.py
+++ src/fairscape_cli/models/utils.py
@@ -10,13 +10,14 @@ import datetime
 from fairscape_cli.config import (
     NAAN
     )
+import random
 
 squids = Sqids(min_length=6)
 
 def GenerateDatetimeSquid():
     try:
         timestamp_int = int(datetime.datetime.now(datetime.UTC).timestamp())
-        sq = squids.encode([timestamp_int])
+        sq = squids.encode([timestamp_int, random.randint(0, 10000)])
     except: 
         timestamp_int = int(datetime.datetime.utcnow().timestamp())
         sq = squids.encode([timestamp_int])
