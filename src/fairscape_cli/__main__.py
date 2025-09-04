9d9d2b21cce7e05298450b38faa20b9e51feacef
jniestroy <jniestroy@gmail.com>
2025-09-04 10:05:39 -0400

    simplify CLI

--- src/fairscape_cli/__main__.py
+++ src/fairscape_cli/__main__.py
@@ -5,7 +5,6 @@ from fairscape_cli.commands.rocrate_commands import rocrate_group
 from fairscape_cli.commands.import_commands import import_group
 from fairscape_cli.commands.build_commands import build_group
 from fairscape_cli.commands.publish_commands import publish_group
-from fairscape_cli.commands.release_commands import release_group
 from fairscape_cli.commands.schema_commands import schema
 from fairscape_cli.commands.validate_commands import validate_group
 from fairscape_cli.commands.augment_commands import augment_group
@@ -25,7 +24,6 @@ cli.add_command(rocrate_group, name='rocrate')
 cli.add_command(import_group, name='import')
 cli.add_command(build_group, name='build')
 cli.add_command(publish_group, name='publish')
-cli.add_command(release_group, name='release')
 cli.add_command(schema, name='schema')
 cli.add_command(validate_group, name='validate')
 cli.add_command(augment_group, name='augment')
